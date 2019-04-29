43e0c8487b6dfe373cebd592cdd475c8c657360c
Sasha <sdrozdiuk12@gmail.com>
2019-04-29 20:47:07 +0300

    Fu Danylo
    :)

--- LD44/Assets/Scripts/Game/MapGenerate.cs
+++ LD44/Assets/Scripts/Game/MapGenerate.cs
@@ -4,31 +4,25 @@ using UnityEngine;
 
 public class MapGenerate : MonoBehaviour
 {
-    public GameObject DefaultObject;
+    public GameObject MainObject;
     public GameObject ObjectForSpawn;
 
     void Start()
     {
-
         var obj = ObjectForSpawn.GetComponent<SpriteRenderer>();
         for (float i = -obj.bounds.size.x; i <= obj.bounds.size.x; i += obj.bounds.size.x)
         {
             for (float j = -obj.bounds.size.y; j <= obj.bounds.size.y; j += obj.bounds.size.y)
             {
-                //if (i == obj.bounds.size.x && j == obj.bounds.size.y)
-                //{
+                if (i == 0 && j == 0)
+                {
+                    Instantiate(MainObject, new Vector3(i, j, 0), new Quaternion());
+                }
+                else
+                {
                     Instantiate(ObjectForSpawn, new Vector3(i, j, 0), new Quaternion());
-                //}
-                //else
-                //{
-                   // Instantiate(ObjectForSpawn, new Vector3(i, j, 0), new Quaternion());
-               // }
+                }
             }
         }
-    }
-
-    void Update()
-    {
-
     }
 }
