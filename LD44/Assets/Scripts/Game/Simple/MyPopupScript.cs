1e94ebd7a2a92443686d7d4cb0c11e6898da35b3
Sasha <sdrozdiuk12@gmail.com>
2019-04-29 17:14:11 +0300

    MyTry
    123

---
+++ LD44/Assets/Scripts/Game/Simple/MyPopupScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MyPopupScript : MonoBehaviour
+{
+    private bool MouseOver = false;
+    public Object ObjectToView; 
+
+    public void IsMouseOverUI()
+    {
+        MouseOver = true;
+        Instantiate(ObjectToView);  
+    }
+        
+    public void OnMoseExit()
+    {
+
+    }
+
+}
+    
