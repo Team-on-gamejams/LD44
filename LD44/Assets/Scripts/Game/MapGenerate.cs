1b982e7349bad31aa1d8de3e9951540380e5b35d
Sasha <sdrozdiuk12@gmail.com>
2019-04-29 23:22:30 +0300

    Zadolbali eti Tail'u esli 4esno
    Zadolbali eti Tail'u esli 4esno

--- LD44/Assets/Scripts/Game/MapGenerate.cs
+++ LD44/Assets/Scripts/Game/MapGenerate.cs
@@ -13,35 +13,19 @@ public class MapGenerate : MonoBehaviour
         {
             for (float j = -obj.bounds.size.y; j <= obj.bounds.size.y; j += obj.bounds.size.y)
             {
-                if (i == 0 && j == 0)
+                if(i== -obj.bounds.size.x && j == -obj.bounds.size.y)
                 {
                     Instantiate(Objects[0], new Vector3(i, j, 0), new Quaternion());
+                }
+                else if (i == 0 && j == 0)
+                {
+                    Instantiate(Objects[3], new Vector3(i, j, 0), new Quaternion());
                 }
                 else
                 {
                     Instantiate(Objects[Random.Range(1,5)], new Vector3(i, j, 0), new Quaternion());
                 }
             }
-        }
-
-
-
-
-
-        //var obj = ObjectForSpawn.GetComponent<SpriteRenderer>();
-        //for (float i = -obj.bounds.size.x; i <= obj.bounds.size.x; i += obj.bounds.size.x)
-        //{
-        //    for (float j = -obj.bounds.size.y; j <= obj.bounds.size.y; j += obj.bounds.size.y)
-        //    {
-        //        if (i == 0 && j == 0)
-        //        {
-        //            Instantiate(MainObject, new Vector3(i, j, 0), new Quaternion());
-        //        }
-        //        else
-        //        {
-        //            Instantiate(ObjectForSpawn, new Vector3(i, j, 0), new Quaternion());
-        //        }
-        //    }
-        //}
+        }    
     }
 }
