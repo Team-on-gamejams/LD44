8b97b90bcccc26172812d8e2908716667079c698
Sasha <sdrozdiuk12@gmail.com>
2019-04-29 23:11:43 +0300

    + new Tiles spawn
    123

--- LD44/Assets/Scripts/Game/MapGenerate.cs
+++ LD44/Assets/Scripts/Game/MapGenerate.cs
@@ -4,25 +4,44 @@ using UnityEngine;
 
 public class MapGenerate : MonoBehaviour
 {
-    public GameObject MainObject;
-    public GameObject ObjectForSpawn;
+    public GameObject[] Objects;
 
     void Start()
     {
-        var obj = ObjectForSpawn.GetComponent<SpriteRenderer>();
+        var obj = Objects[0].GetComponent<SpriteRenderer>();
         for (float i = -obj.bounds.size.x; i <= obj.bounds.size.x; i += obj.bounds.size.x)
         {
             for (float j = -obj.bounds.size.y; j <= obj.bounds.size.y; j += obj.bounds.size.y)
             {
                 if (i == 0 && j == 0)
                 {
-                    Instantiate(MainObject, new Vector3(i, j, 0), new Quaternion());
+                    Instantiate(Objects[0], new Vector3(i, j, 0), new Quaternion());
                 }
                 else
                 {
-                    Instantiate(ObjectForSpawn, new Vector3(i, j, 0), new Quaternion());
+                    Instantiate(Objects[Random.Range(1,5)], new Vector3(i, j, 0), new Quaternion());
                 }
             }
         }
+
+
+
+
+
+        //var obj = ObjectForSpawn.GetComponent<SpriteRenderer>();
+        //for (float i = -obj.bounds.size.x; i <= obj.bounds.size.x; i += obj.bounds.size.x)
+        //{
+        //    for (float j = -obj.bounds.size.y; j <= obj.bounds.size.y; j += obj.bounds.size.y)
+        //    {
+        //        if (i == 0 && j == 0)
+        //        {
+        //            Instantiate(MainObject, new Vector3(i, j, 0), new Quaternion());
+        //        }
+        //        else
+        //        {
+        //            Instantiate(ObjectForSpawn, new Vector3(i, j, 0), new Quaternion());
+        //        }
+        //    }
+        //}
     }
 }
