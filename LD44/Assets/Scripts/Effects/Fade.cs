5e132941af35038b440b97c3f75800f241de1559
Team <timonsol13@gmail.com>
2019-04-30 02:14:21 +0300

    add fade

---
+++ LD44/Assets/Scripts/Effects/Fade.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour {
+	void Start() {
+		GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+		Destroy(this);
+	}
+}
